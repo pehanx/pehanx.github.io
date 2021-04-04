1ab39237dccb787760fb8c332fa828c51bf36dcf
pehanx <nikitos.0@list.ru>
2021-04-05 00:17:34 +0500

    add src

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+import Splide from '@splidejs/splide';
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+        type: 'loop',
+        perPage: 3,
+        rewind: true,
+        arrows: false,
+        breakpoints: {
+            768: {
+                perPage: 2,
+            },
+            480: {
+                perPage: 1
+            }
+        }
+    }).mount();
+
+    const acc = document.getElementsByClassName("accordion");
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function (e) {
+            e.preventDefault();
+            let parent = this.parentElement.parentElement;
+            let content = this.parentElement.nextElementSibling;
+
+            if (!parent.classList.contains('faq__question--open')) {
+                content.classList.add('faq__question-content--padding');
+                parent.classList.add('faq__question--open');
+            } else {
+                parent.classList.remove('faq__question--open');
+                setTimeout(function () {
+                    content.classList.remove('faq__question-content--padding');
+                }, 200);
+            }
+        });
+    }
+
+    const menu_burger = document.querySelector(".header__burger");
+    menu_burger.addEventListener("click", function () {
+        this.classList.toggle('header__burger--open')
+    })
+});
